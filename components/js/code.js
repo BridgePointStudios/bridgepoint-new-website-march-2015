dcce0713b3c544b56a554e4e43aa589c7b61455b
BridgePointStudios <jeremy@bridgepointstudio.com>
2015-03-23 22:06:37 -0500

    rearranged folder structure

---
+++ components/js/code.js
@@ -0,0 +1,10 @@
+$('.carousel').carousel({
+  interval: 4000,
+  pause: "false"
+});
+
+$(document).on('click','.navbar-collapse.in',function(e) {
+    if( $(e.target).is('a') ) {
+        $(this).collapse('hide');
+    }
+});
--- gulpfile.js
+++ gulpfile.js
@@ -2,3 +2,12 @@ var gulp = require('gulp'),
     gutil = require('gulp-util'),
     uglify = require('gulp-uglify'),
     sass = require('gulp-ruby-sass');
+
+// Scripts Task
+// Uglifies
+gulp.task('scripts', function(){
+    return gulp.src('components/js/*.js')
+               .pipe(uglify()
+                 .on('error', gutil.log))
+               .pipe(gulp.dest('builds/production/js'));
+});

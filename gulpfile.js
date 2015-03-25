470d5bbe3af3cd42751b1bce9819ecdb4ad0a917
BridgePointStudios <jeremy@bridgepointstudio.com>
2015-03-25 13:32:34 -0500

    added LiveReload

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,16 @@ var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
     concat = require('gulp-concat'),
     browserify = require('gulp-browserify'),
-    compass = require('gulp-compass')
+    compass = require('gulp-compass'),
+    connect = require('gulp-connect');
 
 // Scripts Task
 // Uglifies
 gulp.task('scripts', function(){
     return gulp.src('components/js/*.js')
                .pipe(browserify())
-               .pipe(gulp.dest('builds/development/js'));
+               .pipe(gulp.dest('builds/development/js'))
+               .pipe(connect.reload());
 });
 
 // Styles Task
@@ -22,12 +24,27 @@ gulp.task('styles', function(){
             images: 'builds/development/images',
             style: 'expanded'
         }))
-        .pipe(gulp.dest('builds/development/css'));
+        .pipe(gulp.dest('builds/development/css'))
+        .pipe(connect.reload());
 });
 
+// Watch Task
+// Watches SCSS and JS folders
 gulp.task('watch', function() {
         gulp.watch('components/js/*.js', ['scripts']);
-        gulp.watch('components/scss/*.scss', ['styles'])
+        gulp.watch('components/scss/*.scss', ['styles']);
+
 });
 
-gulp.task('default', ['styles', 'scripts', 'watch']);
+
+// Connect Task
+// Live Reload
+gulp.task('connect', function(){
+    connect.server({
+        root: 'builds/development/',
+        livereload: true
+    });
+});
+
+// Default task runner
+gulp.task('default', ['styles', 'scripts', 'watch', 'connect']);

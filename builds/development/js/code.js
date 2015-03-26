595744de8d0407413edaf148daa3dbaa71bb5656
BridgePointStudios <jeremy@bridgepointstudio.com>
2015-03-26 02:03:35 -0500

    adding scrollspy.js

--- builds/development/js/code.js
+++ builds/development/js/code.js
@@ -1,7 +1,33 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-$('.carousel').carousel({
-  interval: 4000,
-  pause: "false"
+$(function() {
+    "use strict"
+
+    $('.carousel').carousel({
+      interval: 4000,
+      pause: "false"
+    });
+
+    var topoffset = 500;
+    $('body').scrollspy({
+            target: 'header .navbar',
+            offset: topoffset
+    });
+
+    var hash = $(this).find('li.active a').attr('href');
+    if(hash !== '#featured') {
+        $('header nav').addClass('inbody');
+    } else {
+        $('header nav').removeClass('inbody');
+    }
+
+    $('.navbar-fixed-top').on('activate.bs.scrollspy', function() {
+        var hash = $(this).find('li.active a').attr('href');
+        if(hash !== '#featured') {
+            $('header nav').addClass('inbody');
+        } else {
+            $('header nav').removeClass('inbody');
+        }
+    });
 });
 
 $(document).on('click','.navbar-collapse.in',function(e) {
@@ -9,6 +35,5 @@ $(document).on('click','.navbar-collapse.in',function(e) {
         $(this).collapse('hide');
     }
 });
-console.log(' ');
 
 },{}]},{},[1])
--- components/js/code.js
+++ components/js/code.js
@@ -1,6 +1,32 @@
-$('.carousel').carousel({
-  interval: 4000,
-  pause: "false"
+$(function() {
+    "use strict"
+
+    $('.carousel').carousel({
+      interval: 4000,
+      pause: "false"
+    });
+
+    var topoffset = 500;
+    $('body').scrollspy({
+            target: 'header .navbar',
+            offset: topoffset
+    });
+
+    var hash = $(this).find('li.active a').attr('href');
+    if(hash !== '#featured') {
+        $('header nav').addClass('inbody');
+    } else {
+        $('header nav').removeClass('inbody');
+    }
+
+    $('.navbar-fixed-top').on('activate.bs.scrollspy', function() {
+        var hash = $(this).find('li.active a').attr('href');
+        if(hash !== '#featured') {
+            $('header nav').addClass('inbody');
+        } else {
+            $('header nav').removeClass('inbody');
+        }
+    });
 });
 
 $(document).on('click','.navbar-collapse.in',function(e) {
@@ -8,4 +34,3 @@ $(document).on('click','.navbar-collapse.in',function(e) {
         $(this).collapse('hide');
     }
 });
-console.log(' ');
